abbf15bc8c7e5aa0c60aecf66f74a90d06e0d875
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-06-07 03:14:11 +0300

    Editting Statik Default Page

---
+++ RealHouzing.Consume/ViewComponents/Default/_HeadPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealHouzing.Consume.ViewComponents.Default
+{
+    public class _HeadPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealHouzing.Consume/ViewComponents/Default/_ScriptPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealHouzing.Consume.ViewCompanents.Default
+{
+    public class _ScriptPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
